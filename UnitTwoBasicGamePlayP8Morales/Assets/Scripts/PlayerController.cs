77e1da639513fefb2400bbcd44cc787074bf2b2c
DavidPatino8 <719048@student.dusd.net>
2025-04-02 15:26:44 -0700

    Progress
    Made vertical movement. Look at tutorial to know where to start off.

--- UnitTwoBasicGamePlayP8Morales/Assets/Scripts/PlayerController.cs
+++ UnitTwoBasicGamePlayP8Morales/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
-    public float xRange = 10.0f;
+    public float xRange = 20.0f;
+    public float yRange = 10.0f;
 
     public GameObject projectilePrefab;
 
@@ -19,6 +21,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Set up to move on x axis
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -28,10 +31,26 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        // swet up to move on y axis
+        if (transform.position.y > yRange)
+        {
+            transform.position = new Vector3(yRange, transform.position.x, transform.position.z);
+        }
 
+        if (transform.position.y < -yRange)
+        {
+            transform.position = new Vector3(-yRange, transform.position.x, transform.position.z);
+        }
+
+        // to tell how to move horizontally
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
+        // vertical movement
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+        // to shoot sandwich
         if(Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
