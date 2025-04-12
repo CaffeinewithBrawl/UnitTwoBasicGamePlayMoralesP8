a3011fd6c021eb63c14abe0708fc434f19587445
TheWaspsAreHere <719048@student.dusd.net>
2025-04-11 18:25:21 -0700

    progress
    None.

--- UnitTwoBasicGamePlayP8Morales/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ UnitTwoBasicGamePlayP8Morales/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -9,15 +9,14 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXLeft = -22;
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
-    private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+   
 
     private float delayBeforeSpawn = 3.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        
     }
 
     private void Update()
